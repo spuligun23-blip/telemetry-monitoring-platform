e93d8c4f4670cabd7c94b8a427200881b337aa38
Sandesh Puligundla <spuligun23@gmail.com>
2026-02-03 20:48:07 -0800

    Trigger workflow re-run

--- databricks-bundles/src/hello.py
+++ databricks-bundles/src/hello.py
@@ -1,4 +1,4 @@
 # Databricks notebook source
 print("Hello from Databricks!")
 print(f"Workspace: {spark.conf.get('spark.databricks.workspaceId')}")
-print("✅ Deployment successful via GitHub Actions + DABs! Lets check it out")
+print("✅ Deployment successful via GitHub Actions + DABs! Lets check it out!")
